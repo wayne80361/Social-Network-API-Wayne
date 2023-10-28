ba2daf62a15b6ffbd812dd96a9b40167f5bfb52c
wayne80361 <wayne80361@gmail.com>
2023-10-28 11:45:49 -0400

    build User Model based on the challenge instruction.

---
+++ models/User.js
@@ -0,0 +1,46 @@
+const { Schema, model } = require("mongoose");
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      match: /^[\w-]+(\.[\w-]+)*@[\w-]+(\.[\w-]+)+$/, // email validation using regex
+    },
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Thought", // reference to the Thought model
+      },
+    ],
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "User", // self-referential reference to the User model
+      },
+    ],
+  },
+  {
+    toJSON: {
+      // mongoose does not include virtuals by default, so add a `virtuals` property and set it's value to true
+      virtuals: true,
+    },
+    id: false,
+  }
+);
+
+// create a virtual to retrieve the friendCount
+userSchema.virtual("friendCount").get(function () {
+  return this.friends.length;
+});
+
+const User = model("User", userSchema);
+
+module.exports = User;
